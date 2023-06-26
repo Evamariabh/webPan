2ee6d6e609d36a5050116691345a45cb7da8dd4e
evamariabh <20200783@uthh.edu.mx>
2023-06-25 23:18:27 -0600

    first commit

---
+++ panaderiaWeb/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { Routes } from '@angular/router';
+
+const routes: Routes =[
+  {path:'', pathMatch:'full', redirectTo:''},
+  {path:'login', component:LoginComponent },
+  {path:'singIn', component:RegistroComponent },
+    
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    LoginComponent,
+    RegistroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
